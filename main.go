3f6253c8a941e639e89aea483b69392b238b2400
Maite Capistrano Refosco <maite.refosco@eldorado.org.br>
2023-10-17 10:49:05 -0300

    estudos orientacao objeto com go

---
+++ main.go
@@ -0,0 +1,81 @@
+package main
+
+import "fmt"
+
+type ContaCorrente struct {
+	titular       string
+	numeroAgencia int
+	numeroConta   int
+	saldo         float64
+}
+
+func (c *ContaCorrente) Sacar(valorSaque float64) string {
+	podeSacar := valorSaque > 0 && valorSaque <= c.saldo
+
+	if podeSacar {
+		c.saldo -= valorSaque
+		return "Saque realizado com sucesso!"
+	} else {
+		return "Saldo insuficiente."
+	}
+}
+
+func (c *ContaCorrente) Depositar(valorDeposito float64) (string, float64) {
+	if valorDeposito > 0 {
+		c.saldo += valorDeposito
+		return "Deposito realizado com sucesso. Saldo:", c.saldo
+	} else {
+		return "Valor inválido para depósito Saldo:", c.saldo
+	}
+}
+
+func main() {
+
+	conta01 := ContaCorrente{
+		titular:       "Pafuncio",
+		numeroAgencia: 123,
+		numeroConta:   01,
+	}
+	fmt.Println(conta01)
+
+	conta02 := ContaCorrente{
+		titular:       "Pafuncio",
+		numeroAgencia: 123,
+		numeroConta:   01,
+	}
+	fmt.Println(conta02)
+
+	fmt.Println(conta01 == conta02) //mesmo que os endereços sejam diferentes, o go consegue identificar o conteúdo do objeto
+
+	//conta02 := ContaCorrente{"Marisa", 123, 02, 100.40} //usar para ter todos os campos preenchidos
+	//fmt.Println(conta02)
+
+	var conta03 *ContaCorrente //ponteiro para a struct ContaCorrente,o print retornará com um &
+	conta03 = new(ContaCorrente)
+	conta03.titular = "Aristoteles"
+	fmt.Println(&conta03) //usar o * para obter o conteúdo que há no ponteiro/endereço de memória
+
+	var conta04 *ContaCorrente
+	conta04 = new(ContaCorrente)
+	conta04.titular = "Aristoteles"
+	fmt.Println(&conta04) //usar o * para obter o conteúdo que há no ponteiro/endereço de memória
+
+	fmt.Println(conta03 == conta04) //endereços diferentes = false
+
+	fmt.Println("===============================================================================================")
+
+	conta05 := ContaCorrente{}
+	conta05.titular = "Penelope"
+	conta05.saldo = 500
+	fmt.Println(conta05)
+
+	status, valor := conta05.Depositar(2000)
+	fmt.Println(status, valor)
+
+	fmt.Println(conta05.Sacar(-100))
+	fmt.Println(conta05.saldo)
+
+	fmt.Println(conta05.Depositar(400))
+	fmt.Println(conta05.saldo)
+
+}
